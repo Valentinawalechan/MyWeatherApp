f27a0ce3b5255b32b12b4a751e7aa5ff0e995293
Valentinawalechan <vale.disa86@gmail.com>
2021-04-26 15:53:53 +0100

    adjusted css + created forecast

--- index.js
+++ index.js
@@ -1,33 +1,4 @@
-let currentday = document.querySelector(".date");
-let now = new Date();
-let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
-let months = [
-  "Jan",
-  "Feb",
-  "Mar",
-  "Apr",
-  "May",
-  "Jun",
-  "Jul",
-  "Aug",
-  "Sep",
-  "Oct",
-  "Nov",
-  "Dec"
-];
-let day = days[now.getDay()];
-let date = now.getDate();
-let month = months[now.getMonth()];
-let hour = now.getHours();
-if(hour <10){
-  hour = `0${hour}`
-};
-let minutes = now.getMinutes();
-if(minutes <10) {
-  minutes =`0${minutes}`
-}
-///sunrise sunset
-function showUpdate(timestamp) {
+function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
   if (hours < 10) {
@@ -38,32 +9,75 @@ function showUpdate(timestamp) {
     minutes = `0${minutes}`;
   }
 
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+function showUpdate(timestamp) {
+  let time= new Date(timestamp);
+  let hours = time.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = time.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
   return `${hours}:${minutes}`;
 }
 
-let currentDate = document.querySelector(".date");
-currentDate.innerHTML = `${day} ${date} ${month}, ${hour}:${minutes}`;
+
 //// FORECAST
-function showForecast() {
+function showForecast(response) {
+  let forecast = response.data.daily;
   let forecastElement=document.querySelector("#forecast-temp");
   let forecastHTML = `<div class="row">`;
-  let days = ["Sat", "Sun", "Mon", "Tue", "Wed"];
-  days.forEach(function(day) {
+  
+  forecast.forEach(function(forecastDay, index) {
+    if(index <5) {
   forecastHTML = forecastHTML + `
             <div class="col">
                 <div class="card">
-                    <span class="day-1">${day}</span>
-                    <img src="http://openweathermap.org/img/wn/50d@2x.png" width="80px" />
+                    <span class="day-1">${formatDay(forecastDay.dt)}</span>
+                    <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" width="80px" />
                     <div class="card-body">
-                        <p class="card-text"> 11°/4°
+                        <p class="card-text">${Math.round(
+            forecastDay.temp.max
+          )}/${Math.round(
+            forecastDay.temp.min
+          )}°
                         </p>
                     </div>
                 </div>
             </div>`;
+          }
   })
   
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "4afbca1ce928a5e5a3ce7cd865810e81";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showForecast);
 }
 // Search for a city
 
@@ -77,18 +91,19 @@ document.querySelector(".dayForecast").innerHTML =  response.data.weather[0].des
 document.querySelector("#max").innerHTML = `${Math.round(response.data.main.temp_max)}°`;
 document.querySelector("#min").innerHTML = `/${Math.round(response.data.main.temp_min)}°`;
 
-document.querySelector(".sunrise").innerHTML = `${showUpdate(response.data.sys.sunrise * 1000)} AM`;
-document.querySelector(".sunset").innerHTML = `${showUpdate(response.data.sys.sunset * 1000)} PM`;
+document.querySelector(".sunrise").innerHTML = `${showUpdate(response.data.sys.sunrise*1000)} AM`;
+document.querySelector(".sunset").innerHTML = `${showUpdate(response.data.sys.sunset*1000)} PM`;
 
 let iconElement = document.querySelector("#icon");
 iconElement.setAttribute(`src`, `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
-icon.Element.setAttribute("alt", response.data.weather[0].description);
+iconElement.setAttribute(`alt`, response.data.weather[0].description);
+getForecast(response.data.coord);
 }
 
 function search(city) {
 let apiKey = "4afbca1ce928a5e5a3ce7cd865810e81";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showTemp);
+axios.get(apiUrl).then(showTemp);
 }
 
 
@@ -152,4 +167,3 @@ celsiusLink.addEventListener("click", showCelsius);
 
 
 search("London");
-showForecast();
